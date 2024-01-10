723e2aee92b4dbdf6275e75874fb34e4ed5af655
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-01-10 18:38:51 +0100

    Update clicktheplanet-v2.sql

--- clicktheplanet-v2.sql
+++ clicktheplanet-v2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2024 at 07:14 AM
+-- Generation Time: Jan 10, 2024 at 06:38 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -33,15 +33,60 @@ CREATE TABLE `game` (
   `diamonds` varchar(50) NOT NULL,
   `currentlevel` varchar(50) NOT NULL,
   `maxlevel` varchar(50) NOT NULL,
-  `stage` int(2) NOT NULL
+  `currentstage` int(2) NOT NULL,
+  `maxstage` int(2) NOT NULL,
+  `guild_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `game`
 --
 
-INSERT INTO `game` (`id`, `gold`, `diamonds`, `currentlevel`, `maxlevel`, `stage`) VALUES
-(11, '2137', '120', '17', '17', 8);
+INSERT INTO `game` (`id`, `gold`, `diamonds`, `currentlevel`, `maxlevel`, `currentstage`, `maxstage`, `guild_id`) VALUES
+(10, '420', '999', '1', '1', 9, 0, 0),
+(11, '2137', '120', '23', '23', 0, 0, 0),
+(12, '100', '100', '18', '18', 3, 3, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guilds`
+--
+
+CREATE TABLE `guilds` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `owner` int(11) NOT NULL,
+  `messages_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guilds`
+--
+
+INSERT INTO `guilds` (`id`, `name`, `owner`, `messages_id`) VALUES
+(3, 'Galaxy conquerors', 11, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(50) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `message` text NOT NULL,
+  `date_sent` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `guild_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `user`, `message`, `date_sent`, `guild_id`) VALUES
+(1, 'Qwe', 'Hello world', '2024-01-10 17:37:25', 3);
 
 -- --------------------------------------------------------
 
@@ -63,9 +108,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `login`, `password`, `admin`, `game_id`) VALUES
-(10, 'hello@mail.com', 'Hello', '$2a$10$0WrhDWNoA2IQL6J7aGOA2O3uYkz9Tc/eEjXjjumMtvhcGb.KIrBRW', 1, 0),
+(10, 'hello@mail.com', 'Hello', '$2a$10$0WrhDWNoA2IQL6J7aGOA2O3uYkz9Tc/eEjXjjumMtvhcGb.KIrBRW', 1, 10),
 (11, 'qwe@mail.com', 'Qwe', '$2a$10$lINez2/4OXVjAVtY5Ajr/uw/S5vaIPGq.JCPq6Hki0SkmIbXSblRC', 0, 11),
-(12, 'moststrongestpotato@mail.com', 'MostStrongestPotato', '$2a$10$MSNS3Qq8/744On2MeolpTOyu6fvCnxIS3A8BNVMBNpJ8.i/koUEW.', 0, 0);
+(12, 'moststrongestpotato@mail.com', 'MostStrongestPotato', '$2a$10$MSNS3Qq8/744On2MeolpTOyu6fvCnxIS3A8BNVMBNpJ8.i/koUEW.', 0, 12);
 
 --
 -- Indexes for dumped tables
@@ -78,10 +123,24 @@ ALTER TABLE `game`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `guilds`
+--
+ALTER TABLE `guilds`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `messages_id` (`messages_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `game_id` (`game_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -91,7 +150,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `game`
 --
 ALTER TABLE `game`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `guilds`
+--
+ALTER TABLE `guilds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
