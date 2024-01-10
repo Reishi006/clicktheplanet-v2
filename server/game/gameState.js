2003d7c3398263295378238ccd155a72f9b4abb5
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-01-10 07:13:48 +0100

    Update gameState.js

--- server/game/gameState.js
+++ server/game/gameState.js
@@ -17,9 +17,41 @@ process.on('currentUserIdSet', (userId) => {
     console.log(`currentUserIdSet: ${userId}`); //global variable for connected user
 });
 
-
-
 let resetPlanet = false;
+
+
+const advancePlanet = () => {
+    gameState.planet.stage = gameState.planet.stage + 1;
+    if (gameState.planet.stage > 10) {
+        gameState.planet.stage = 0;
+        gameState.planet.currentLevel = gameState.planet.currentLevel + 1;
+        if (gameState.planet.currentLevel >= gameState.planet.maxLevel) gameState.planet.maxLevel = gameState.planet.currentLevel;
+
+        db.query('UPDATE game JOIN users ON game.id = users.game_id SET currentlevel = ?, maxlevel = ?, stage = ? WHERE users.id = ?', 
+        [gameState.planet.currentLevel, gameState.planet.maxLevel, gameState.planet.stage, userId], (err, data) => {
+            if (err) throw err;
+            console.log('%c levels updated', 'color: lightgreen');
+        });
+    } else {
+        db.query('UPDATE game JOIN users ON game.id = users.game_id SET stage = ? WHERE users.id = ?', 
+        [gameState.planet.stage, userId], (err, data) => {
+            if (err) throw err;
+            console.log('%c stage updated', 'color: lightblue');
+        });
+    }
+}
+
+const hitPlanet = () => {
+    gameState.planet.currentHp -= 10;
+    if (gameState.planet.currentHp <= 0) {
+        gameState.planet.currentHp = gameState.planet.maxHp;
+        resetPlanet = true;
+        advancePlanet();
+    } else {
+        resetPlanet = false;
+    }
+}
+
 
 module.exports = {
     getIo: (server) => {
@@ -47,13 +79,7 @@ module.exports = {
 
             socket.on('sendclick', () => {
                 socket.emit('receiveclick', {gameState: gameState, resetPlanet: resetPlanet});
-                gameState.planet.currentHp -= 10;
-                if (gameState.planet.currentHp <= 0) {
-                    gameState.planet.currentHp = gameState.planet.maxHp;
-                    resetPlanet = true;
-                } else {
-                    resetPlanet = false;
-                }
+                hitPlanet();
             });
         });
 
