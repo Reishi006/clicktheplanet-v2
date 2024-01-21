650949b58ac5b6f6d7548386210c1529cbba6ef5
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-01-21 09:10:44 +0100

    Update DrawerAdmin.jsx

--- client/src/components/DrawerAdmin.jsx
+++ client/src/components/DrawerAdmin.jsx
@@ -1,7 +1,7 @@
 import { store, ship, stats, guild, wheel, admin, } from '../assets/img-import.js';
 
 
-export default function Drawer({ setShow, openChat }) {
+export default function DrawerAdmin({ setShow, openChat }) {
 
     const twoFunctions = () => {
         setShow(3); 
@@ -12,10 +12,10 @@ export default function Drawer({ setShow, openChat }) {
         <div className='drawer-container'>
             <div className='drawer-element' onClick={() => setShow(0)}><img src={store} className='drawer-icon' alt='store'></img></div>
             <div className='drawer-element' onClick={() => setShow(1)}><img src={ship} className='drawer-icon' alt='ship'></img></div>
-            <div className='drawer-element' onClick={() => setShow(2)}><img src={stats} className='drawer-icon' alt='stats'></img></div>
+            <div className='drawer-element' onClick={() => setShow(2)}><img src={stats} className='drawer-icon' alt='stats'></img></div>{/* 
             <div className='drawer-element' onClick={() => twoFunctions()}><img src={guild} className='drawer-icon' alt='guild'></img></div>
-            <div className='drawer-element' onClick={() => setShow(4)}><img src={wheel} className='drawer-icon' alt='wheel'></img></div>
-            <div className='drawer-element' onClick={() => setShow(5)}><img src={admin} className='drawer-icon' alt='admin'></img></div>
+            <div className='drawer-element' onClick={() => setShow(4)}><img src={wheel} className='drawer-icon' alt='wheel'></img></div> */}
+            <div className='drawer-element' onClick={() => setShow(3)}><img src={admin} className='drawer-icon' alt='admin'></img></div>
         </div>
     )
 }
