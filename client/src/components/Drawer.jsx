d3b212647c075285a6c9578ab88b8a52fb6294ef
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2024-01-21 09:10:34 +0100

    Update Drawer.jsx

--- client/src/components/Drawer.jsx
+++ client/src/components/Drawer.jsx
@@ -13,8 +13,8 @@ export default function Drawer({ setShow, openChat }) {
             <div className='drawer-element' onClick={() => setShow(0)}><img src={store} className='drawer-icon' alt='store'></img></div>
             <div className='drawer-element' onClick={() => setShow(1)}><img src={ship} className='drawer-icon' alt='ship'></img></div>
             <div className='drawer-element' onClick={() => setShow(2)}><img src={stats} className='drawer-icon' alt='stats'></img></div>
-            <div className='drawer-element' onClick={() => twoFunctions()}><img src={guild} className='drawer-icon' alt='guild'></img></div>
-            <div className='drawer-element' onClick={() => setShow(4)}><img src={wheel} className='drawer-icon' alt='wheel'></img></div>
+            {/* <div className='drawer-element' onClick={() => twoFunctions()}><img src={guild} className='drawer-icon' alt='guild'></img></div>
+            <div className='drawer-element' onClick={() => setShow(4)}><img src={wheel} className='drawer-icon' alt='wheel'></img></div> */}
         </div>
     )
 }
